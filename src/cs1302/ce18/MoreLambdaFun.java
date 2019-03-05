68e114336ff1828a7aa0bb9868941bfa6c49c2d5
Michael Cotterell <mepcotterell@gmail.com>
2019-03-05 08:51:35 -0500

    Update MoreLambdaFun.java

--- src/cs1302/ce18/MoreLambdaFun.java
+++ src/cs1302/ce18/MoreLambdaFun.java
@@ -54,7 +54,7 @@ public class MoreLambdaFun {
      * indices will exist if, and only if, {@code l} is greater than 
      * {@code a.length}. The resulting array is created using {@code g}.
      *
-     * @param <R> the type of the array elements.
+     * @param <T> the type of the array elements.
      * @param a the array to be copied
      * @param l the length of the copy to be returned
      * @param g a function which produces a new array of the desired type and length
@@ -62,7 +62,7 @@ public class MoreLambdaFun {
      * @throws NegativeArraySizeException if {@code l} is negative
      * @throws NullPointerException if {@code a} or {@code g} is {@code null}
      */
-    public static <R> R[] copyOf(R[] a, int l, IntFunction<R[]> g) {
+    public static <T> T[] copyOf(T[] a, int l, IntFunction<T[]> g) {
 	throw new UnsupportedOperationException("not yet implemented");
     } // copyOf
 
