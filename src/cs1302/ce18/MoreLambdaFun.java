d32ae8eaafabee76ae8ebcc2d0d010b43848101c
Michael Cotterell (Supa' Mike) <mepcotterell@gmail.com>
2019-03-05 06:12:48 -0500

    cleaned up some formatting in the starter code

--- src/cs1302/ce18/MoreLambdaFun.java
+++ src/cs1302/ce18/MoreLambdaFun.java
@@ -28,28 +28,31 @@ public class MoreLambdaFun {
     } // main    
     
     /**
-     * Prints the elements of the array that pass the test specified by the given predicate
-     * using a string mapper. More formally, this method prints the string mapped elements 
-     * {@code f.apply(e)} in the array referred to by {@code t} for each {@code e} such that 
-     * {@code p.test(e)}. Each string mapped element will be printed on its own line.
+     * Prints the elements of the array that pass the test specified by the
+     * given predicate using a string mapper. More formally, this method prints
+     * the string mapped elements {@code f.apply(e)} in the array referred to by
+     * {@code t} for each {@code e} such that {@code p.test(e)}. Each string 
+     * mapped element will be printed on its own line.
      *
      * @param <T> the type of the array elements
      * @param t the specified array
      * @param p the specified predicate
      * @param p the specified string mapper
-     * @throws NullPointerException if the specified predicate or string mapper is {@code null}
+     * @throws NullPointerException if the specified predicate or string mapper 
+     *                              is {@code null}
      */
     private static <T> void printlnMappedMatches(T[] t, Predicate<T> p, Function<T, String> f) {
         throw new UnsupportedOperationException("not yet implemented");
     } // printlnMappedMatches    
     
     /**
-     * Returns a copy of the specified array, truncating or padding with nulls (if necessary) so 
-     * that the copy has the specified length. For all indices that are valid in both the original 
-     * array and the copy, the two arrays will contain identical values. For any indices that are 
-     * valid in the copy but not the original, the copy will contain {@code null}. Such indices will
-     * exist if, and only if, {@code l} is greater than {@code a.length}. The 
-     * resulting array is created using {@code g}.
+     * Returns a copy of the specified array, truncating or padding with nulls
+     * (if necessary) so that the copy has the specified length. For all 
+     * indices that are valid in both the original array and the copy, the two 
+     * arrays will contain identical values. For any indices that are valid in
+     * the copy but not the original, the copy will contain {@code null}. Such 
+     * indices will exist if, and only if, {@code l} is greater than 
+     * {@code a.length}. The resulting array is created using {@code g}.
      *
      * @param <R> the type of the array elements.
      * @param a the array to be copied
